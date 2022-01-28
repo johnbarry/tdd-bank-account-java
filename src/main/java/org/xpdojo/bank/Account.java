c9293ff4aaa17bae44bb16a6729e6b14630f447e
johnbarry <john.barry@me.com>
2022-01-28 07:57:59 -0500

    Refactored to use emptyAccount static function

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,6 +1,12 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public static Account emptyAccount() {
+        return new Account();
+    }
+    public Account() {
+
+    }
     public int balance() {
         return 0;
     }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,16 +1,15 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xpdojo.bank.Account.emptyAccount;
 
 public class AccountTest {
 
     @Test
 
     public void initialAmount_shouldHaveZeroBalance() {
-        Account account = new Account();
-        assertThat(account.balance()).isEqualTo(0);
+        assertThat(emptyAccount().balance()).isEqualTo(0);
     }
 }
